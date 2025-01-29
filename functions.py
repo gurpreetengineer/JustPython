4e780536e9d1e0899a3ba03c00781908841aa3b6
gurpreetengineer <gb.gurpreetbamrah@gmail.com>
2025-01-29 09:34:25 +0400

    some new files

--- functions.py
+++ functions.py
@@ -5,4 +5,48 @@ def aFunc(newVal2):
 
 
 x = lambda a, b, c: a + b + c()
-print(x(5, 6, aFunc(2)))
+print(x(5, 6, aFunc(2)));
+
+
+def unknownArgsFunc(**doubleStar):
+  return f'{doubleStar["keyIsMust"]} {doubleStar['ItBehavesLikeAnObjectNow']}';
+
+print(unknownArgsFunc(keyIsMust = 'Because it is named args now and it is using \'=\' as default', ItBehavesLikeAnObjectNow = 'As We don\'t know how many args will be here.'));
+
+# Positional arguments means DIRECT VALUES - THIS FUNCTION WON'T WORK
+# def my_function(x, /):
+#   print(x)
+
+# my_function(x = 3)
+
+# THIS WILL WORK. LOOK HOW WE GAVE DIRECT VALUE =====> `, /`
+def my_function(x, /):
+  print(x)
+
+my_function(3)
+
+
+# THIS WILL ALSO WORK
+def my_function(x):
+  print(x)
+
+my_function(x = 33)
+
+
+# OPPOSITE OF POSITIONAL ARGUMENTS: 
+# 
+# 
+# 
+# 	FUNCTIONAL ARGUMENTS ARE THE ONES WHERE YOU NEED TO SPECIFY ARGUMENT NAME. ======> `*, `
+def my_function(*, x):
+  print(x)
+
+my_function(x = 3)
+
+
+# COMBINATION OF BOTH AND NORMAL ARGUMENT
+def my_function(a, b, /, f, g, *, c, d):
+  print(a + b + c + d + f + g)
+
+my_function(5, 6, 232, g = 500, c = 7, d = 8)
+
---
+++ snippets/installAndUseLibraryDirectly.py
@@ -0,0 +1,5 @@
+# import subprocess
+# import sys
+# import importlib
+
+# def install_and_use_package()
